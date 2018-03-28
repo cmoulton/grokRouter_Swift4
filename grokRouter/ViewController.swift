59a45af68a3c09e7dc7fa9ba3b97ee360cb26e72
Christina Moulton <christina@teakmobile.com>
2018-03-28 16:15:16 -0400

    Setup router and simple API calls (no Todo struct yet)

--- grokRouter/ViewController.swift
+++ grokRouter/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -15,11 +16,75 @@ class ViewController: UIViewController {
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func makeGetCallWithAlamofire() {
+    Alamofire.request(TodoRouter.get(1))
+      .responseJSON { response in
+        // check for errors
+        guard response.result.error == nil else {
+          // got an error in getting the data, need to handle it
+          print("error calling GET on /todos/1")
+          print(response.result.error!)
+          return
+        }
+
+        // make sure we got some JSON since that's what we expect
+        guard let json = response.result.value as? [String: Any] else {
+          print("didn't get todo object as JSON from API")
+          if let error = response.result.error {
+            print("Error: \(error)")
+          }
+          return
+        }
+
+        // get and print the title
+        guard let todoTitle = json["title"] as? String else {
+          print("Could not get todo title from JSON")
+          return
+        }
+        print("The title is: " + todoTitle)
+    }
   }
 
+  func makePostCallWithAlamofire() {
+    let newTodo: [String: Any] = ["title": "My first todo", "completed": false, "userId": 1]
+    Alamofire.request(TodoRouter.create(newTodo))
+      .responseJSON { response in
+        guard response.result.error == nil else {
+          // got an error in getting the data, need to handle it
+          print("error calling POST on /todos/1")
+          print(response.result.error!)
+          return
+        }
+        // make sure we got some JSON since that's what we expect
+        guard let json = response.result.value as? [String: Any] else {
+          print("didn't get todo object as JSON from API")
+          if let error = response.result.error {
+            print("Error: \(error)")
+          }
+          return
+        }
+        // get and print the title
+        guard let idNumber = json["id"] as? Int else {
+          print("Could not get id number from JSON")
+          return
+        }
+        print("Created todo with id: \(idNumber)")
+    }
+  }
 
+  func makeDeleteCallWithAlamofire() {
+    Alamofire.request(TodoRouter.delete(1))
+      .responseJSON { response in
+        guard response.result.error == nil else {
+          // got an error in getting the data, need to handle it
+          print("error calling DELETE on /todos/1")
+          if let error = response.result.error {
+            print("Error: \(error)")
+          }
+          return
+        }
+        print("DELETE ok")
+    }
+  }
 }
 
